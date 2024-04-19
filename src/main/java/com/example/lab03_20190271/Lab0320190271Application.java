99c735ae684e96741057aa759c97e8050989404a
Renato Delgado <a20190271@pucp.edu.pe>
2024-04-18 20:46:38 -0500

    primer commit

---
+++ src/main/java/com/example/lab03_20190271/Lab0320190271Application.java
@@ -0,0 +1,13 @@
+package com.example.lab03_20190271;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0320190271Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab0320190271Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab03_20190271/Lab0320190271ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab03_20190271;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0320190271ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
